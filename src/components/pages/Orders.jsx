559171b52354707a5c157c1cd00ad507ec05cfc2
iftakhersifat <hossensifat615@gmail.com>
2025-12-01 20:43:14 +0600

    added orders section

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -36,19 +36,26 @@ export default function Orders() {
   if (!product) return <p className='text-center mt-24'>Loading product...</p>;
 
   return (
-    <div className='max-w-3xl mx-auto px-4 mt-24'>
-      <h1 className='text-3xl font-bold mb-6'>Confirm Your Order</h1>
-      <div className='bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md'>
-        <h2 className='text-xl font-semibold mb-2'>{product.title}</h2>
-        <p className='text-gray-700 dark:text-gray-300 mb-2'>{product.description}</p>
-        <p className='font-semibold mb-4'>Price: ${product.price}</p>
-        <button 
-          onClick={handleConfirmOrder} 
-          className='btn bg-gradient-to-r from-green-500 to-emerald-600 text-white w-full hover:bg-green-700'
-        >
-          Confirm Order
-        </button>
-      </div>
+    <div className="max-w-3xl mx-auto px-6 md:px-6 lg:px-0 mt-12">
+
+    <h1 className="text-3xl md:text-4xl font-bold mb-10 text-center text-gray-900">
+    <span className="bg-linear-to-r from-red-500 to-red-700 bg-clip-text text-transparent">Confirm Your Order</span>
+    </h1>
+
+    <div className="bg-white p-8 rounded-2xl shadow-xl border border-red-200">
+    <h2 className="text-2xl font-semibold mb-4 text-gray-900">{product.title}</h2>
+
+    <div className="w-full h-64 bg-gray-100 rounded-xl mb-6 flex items-center justify-center shadow-md">
+      <img src={product.image} alt={product.title} className="h-full p-4 object-contain"/>
     </div>
+
+    <p className="text-gray-700 mb-4">{product.description}</p>
+
+    <p className="text-xl font-bold text-red-600 mb-6">Price: ${product.price}</p>
+
+    <button onClick={handleConfirmOrder} className="btn w-full py-3 text-lg rounded-xl text-white bg-linear-to-r from-green-500 to-emerald-600 shadow-md">Confirm Order</button>
+  </div>
+</div>
+
   );
 }
